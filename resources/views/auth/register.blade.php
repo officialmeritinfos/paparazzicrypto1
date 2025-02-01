956d5f17a10ebbf65090876099a92e9e7e94e167
Anon Dev <michaelerastus9@gmail.com>
2025-02-01 10:18:41 +0100

    Completed

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <script src='https://www.google.com/recaptcha/api.js'></script>
     <!-- Bootstrap Min CSS -->
     <link rel="stylesheet" href="{{asset('dashboard/user/css/bootstrap.min.css')}}">
     <!-- Owl Theme Default Min CSS -->
@@ -107,6 +107,28 @@
                             <input class="form-control" type="text" placeholder="Enter your Phone"
                                    name="referral" value="{{old('referral')}} {{$referral}}"/>
                         </div>
+                    </div>
+
+                    <div class="row">
+
+                        <div class="col-md-12">
+
+                            <div class="form-group">
+
+                                <strong>ReCaptcha:</strong>
+
+                                <div class="g-recaptcha" data-sitekey="{{ env('GOOGLE_RECAPTCHA_KEY') }}"></div>
+
+                                @if ($errors->has('g-recaptcha-response'))
+
+                                    <span class="text-danger">{{ $errors->first('g-recaptcha-response') }}</span>
+
+                                @endif
+
+                            </div>
+
+                        </div>
+
                     </div>
 
                     <div class="col-12">
